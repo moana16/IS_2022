93ce84f11f15e8971a9fcce50916f8ea4a42587b
jimin <moana16@naver.com>
2022-04-15 22:21:28 +0900

    day7 update

---
+++ day_07/lecture_01.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+fname_input ='./titanic.csv'
+data = pd.read_csv(fname_input,
+                   header = 'infer',
+                   sep=',')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
