466ceb138dee0da9dfc3bb3d21cc483e06403070
jimin <moana16@naver.com>
2022-06-03 10:32:27 +0900

    lecture_07.py update

---
+++ day_13/lecture_07.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+
+from sklearn.datasets import make_moons
+X,y =make_moons(n_samples=200,
+                noise=0.05,
+                random_state=0)
+
+X[:10]
+print(y[:10])
+
+import matplotlib.pyplot as plt
+plt.scatter(X[:,0],X[:,1])
+plt.show()
+
+
+# DBSCAN 클래스 사용
+# - 데이터 간의 밀도를 이용하여 군집의 형성 여부를
+#   결정하는 알고리즘을 구현
+# - DBSCAN은 군집의 크기가 자동으로 결정
+from sklearn.cluster import DBSCAN
+db=DBSCAN(eps=0.2, min_samples=5,metric='euclidean')
+
+y_cluster=db.fit_predict(X)
+
+plt.scatter(X[y_cluster==0, 0],
+            X[y_cluster==0, 1],
+            s=50,
+            c='lightgreen',
+            marker='s',
+            label = 'Cluster 1')
+
+plt.scatter(X[y_cluster==1, 0],
+            X[y_cluster==1, 1],
+            s=50,
+            c='orange',
+            marker='o',
+            label = 'Cluster 2')
+
+
+plt.legend()
+plt.grid()
+plt.show()
+
